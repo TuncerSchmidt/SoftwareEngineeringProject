78db10b2f5a27f5bfcd83758360b586c91aa9370
Tuncer Schmidt <53016813+TuncerSchmidt@users.noreply.github.com>
2022-12-02 10:52:32 -0600

    Update Loading.js

--- yolocode/src/components/Loading.js
+++ yolocode/src/components/Loading.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+//I created loading function
+//It just prints Loading...
 export default function Loading(){
     return <div className="font-bold text-3xl">Loading...</div>
 }
