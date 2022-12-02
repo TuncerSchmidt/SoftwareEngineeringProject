9954a48213023488b3858a8505df5d572ba19195
Tuncer Schmidt <53016813+TuncerSchmidt@users.noreply.github.com>
2022-12-02 11:16:02 -0600

    Update images.js

--- yolocode/src/pages/images.js
+++ yolocode/src/pages/images.js
@@ -5,7 +5,5 @@ import image4 from "../components/images/View1.jpeg";
 import image5 from "../components/images/View2.jpeg";
 import image6 from "../components/images/View3.jpeg";
 
-//All the images are imported
-//Keeping all inside the array
-//It is for slider in about us page
+
 export default [image1, image2, image3, image4, image5, image6];
